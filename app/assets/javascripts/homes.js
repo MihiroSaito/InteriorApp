206298eded75ecf7fe09afe1133bf5b2c9d101fb
MihiroSaito <61680540+MihiroSaito@users.noreply.github.com>
2020-06-30 14:45:18 +0900

    add js

--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -3,17 +3,35 @@ $(function openMenu(){
     $('.open-menu').css({
       'transition-duration': '0.4s',
       position: "absolute",
-      width: "23%",
-      left: "0",
-      'border-right': '2px solid rgb(0, 170, 255)'
-      
+      height: "330px",
+      right: "0",
+      'border-left': '2px solid rgb(0, 170, 255)',
+      'border-bottom': '2px solid rgb(0, 170, 255)'
+    });
+
+    $('.clip-btn').css({
+      'border-top': 'none'
+    });
+
+    $('.menu-contents').css({
+      visibility: 'visible',
+      'transition': '1s',
+      opacity: '1'
     });
 
     $(".clip-btn").on('click', function(){
       $('.open-menu').css({
-        width: "0px",
-        'transition-duration': '0.4s'
+        height: "0px",
+        'transition-duration': '0.4s',
+        'border-bottom': 'none'
       });
+
+      $('.menu-contents').css({
+        visibility: 'hidden',
+        'transition': '0.2s',
+        opacity: '0'
+      });
+
       openMenu();
     });
   });

3050f298223862229948d474278c453bdba065d4
MihiroSaito <61680540+MihiroSaito@users.noreply.github.com>
2020-07-01 13:16:53 +0900

    add routing & login/sigup

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
 
   root "homes#index"
+
+  resources :items, only: [:new, :destroy]
+
+  post "items/new", to: "items#create"
+
+  resources :users, only: :show do
+    member do
+      get "favorite", to: "users#favorite"
+    end
+  end
+
 end
